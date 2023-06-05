9192cae5f9499a4a21546894178c3cd2da90dc93
David Armendariz <darmendariz1998@outlook.com>
2023-06-04 19:27:52 -0500

    Added more functions

---
+++ funciones/regresion.R
@@ -0,0 +1,12 @@
+regresion_lineal <- function(x, y, alpha = 0.05) {
+  model <- lm(y ~ ., data = x)
+  summary_model <- summary(model)
+  coefficients <- coef(summary_model)
+  significant_coefs <- coefficients[coefficients[, 4] < 0.05, ]
+  return(list(
+    model = model,
+    summary_model = summary_model,
+    coefficients = coefficients,
+    significant_coefs = significant_coefs
+  ))
+}
--- regresion/procesadores_regresion.R
+++ regresion/procesadores_regresion.R
@@ -1,6 +1,7 @@
 setwd(".")
 library(lmtest)
 library(readxl)
+source("funciones/regresion.R")
 
 datos <- read_excel("datos/procesadores.xlsx")
 rendimiento_relativo <- datos$`Rendimiento_Relativo`
@@ -8,8 +9,6 @@ rendimiento_relativo_estimado <- datos$`Rendimiento_relativo_estimado`
 x <- datos
 x$`Rendimiento_Relativo` <- NULL
 x$`Rendimiento_relativo_estimado` <- NULL
-model1 <- lm(rendimiento_relativo ~ ., data = x)
-model2 <- lm(rendimiento_relativo_estimado ~ ., data = x)
 
-summary_model1 <- summary(model1)
-summary_model2 <- summary(model2)
+result1 <- regresion_lineal(x, rendimiento_relativo)
+result2 <- regresion_lineal(x, rendimiento_relativo_estimado)
